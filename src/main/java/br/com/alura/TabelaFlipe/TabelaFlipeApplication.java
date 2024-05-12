44ea904223f0a0514201b12b6c48e09ed8b6c030
Jeferson de Souza Andrade <38109358+jefersonae@users.noreply.github.com>
2024-05-12 11:51:49 -0300

    initial project

---
+++ src/main/java/br/com/alura/TabelaFlipe/TabelaFlipeApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.TabelaFlipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabelaFlipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabelaFlipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/TabelaFlipe/TabelaFlipeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.TabelaFlipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabelaFlipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
